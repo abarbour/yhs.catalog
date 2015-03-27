b02d55769eb6c57b5a59d1e79b4c7c6810d6a2e0
Andrew Barbour <abarbour@usgs.gov>
2015-03-27 14:56:21 -0700

    adding formatting function

---
+++ R/utils.R
@@ -0,0 +1,22 @@
+#' Prepare for use with Focal Mechanism Classification
+#' @param Catalog the seismic catalog to use
+#' @param type character; the final format
+#' @references url
+#' @export
+prep_for_fmc <- function(type=NULL){
+  type <- match.arg(type, c('x'))
+  if (!exists('yhs')){
+    env <- new.env()
+    do.call("data", list('yhs', envir=env))
+    yhs <- get('yhs', envir = env)
+  }
+  require(dplyr)
+  if (type=='x'){
+    yhs %>% transmute(Lat.deg, Lon.deg)
+  } else {
+    yhs
+  }
+  
+}
+
+#prep_for_fmc()
