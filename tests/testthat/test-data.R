fea136dc01e8e9926920ef938efb275ddf1965e4
Andrew Barbour <andy.barbour@gmail.com>
2015-08-19 21:59:53 -0700

    Removed useless, unfinished appendage. Added test

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,5 +1,20 @@
-context('Dataset...')
-test_that('tbl_df',{
-	data(yhs)
+context('Main dataset...')
+
+data(yhs)
+yhs.sub <- salton_trough_yhs()
+
+test_that('class is tbl_df',{
 	expect_is(yhs, 'tbl_df')
+  expect_is(yhs.sub, 'tbl_df')
+})
+
+context('Boundaries...')
+
+test_that('Latitudes',{
+  expect_true(max(yhs.sub$Lat.deg) >= 32.5)
+  expect_true(max(yhs.sub$Lat.deg) <= 33.7)
+})
+test_that('Longitudes',{
+  expect_true(min(yhs.sub$Lon.deg) >= -116.3)
+  expect_true(max(yhs.sub$Lon.deg) <= -115.1)
 })
