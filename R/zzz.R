9055e85b77eadfb278af89166135f84132cf6ea6
Andrew Barbour <andy.barbour@gmail.com>
2022-03-20 16:37:47 +0000

    Updated catalogs after a few years of dormancy. Need to add QTM and update CI

--- R/zzz.R
+++ R/zzz.R
@@ -3,9 +3,9 @@
   pack <- "yhs.catalog"
   packv <- utils::packageVersion(pack)
   packvp <- strftime(as.Date(as.character(packv[1,3]), format='%Y%m%d', tz='UTC'), format='%a, %b %d, %Y')
+  msg <- 'refined focal mechanism catalog for southern California'
   packageStartupMessage(
-    sprintf("Loaded %s (%s) -- refined focal mechanism catalog for southern California\n  Note:  %s",
-            pack, packv,
-            paste("* sub-version number shows last catalog update (", packvp, ')'))
+    sprintf("Loaded %s (%s) -- %s\nNotes: 1. %s\n       2. start with `data(yhs)`",
+        pack, packv, msg, paste("* sub-version number shows last catalog update (", packvp, ')'))
   )
 }
--- data-raw/quakes/to.R
+++ data-raw/quakes/to.R
@@ -10,7 +10,8 @@ suppressPackageStartupMessages({
 processor <-  function(fi, src.="", ...){
 	stopifnot(file.exists(fi))
 	message("  --> processing ", fi, "...")
-	read.table(fi, header=TRUE, ...) %>% tbl_df %>% 
+	read.table(fi, header=TRUE, ...) %>% 
+		as_tibble %>% 
 		dplyr::mutate(Source = src.) %>% 
 		dplyr::select(Source, everything())
 }
@@ -21,10 +22,18 @@ if (force.redo | !exists('orig') | !exists('supp11_13') | !exists('supp14')  | !
 	'yhs_supp11_13.hash.gz' %>% processor(src.="supplement_2011-2013") -> supp11_13 
 	'yhs_supp14.hash.gz'    %>% processor(src.="supplement_2014") -> supp14
 	'yhs_supp15.hash.gz'    %>% processor(src.="supplement_2015") -> supp15
-	'yhs_supp16.hash.gz'    %>% processor(src.="supplement_2016(partial)") -> supp16
+	'yhs_supp16.hash.gz'    %>% processor(src.="supplement_2016") -> supp16
+}
+yhsl <- list(orig, supp11_13, supp14, supp15, supp16)
+
+# update March 2022
+if (force.redo | !exists('orig') | !exists('supp17')  | !exists('supp18') | !exists('supp19')){
+	'yhs_supp17.hash.gz'    %>% processor(src.="supplement_2017") -> supp17
+	'yhs_supp18.hash.gz'    %>% processor(src.="supplement_2018") -> supp18
+	'yhs_supp19.hash.gz'    %>% processor(src.="supplement_2019") -> supp19
 }
 
-yhsl <- list(orig, supp11_13, supp14, supp15, supp16)
+yhsl <- c(yhsl, list(supp17, supp18, supp19))
 
 message('  forming full table...')
 dplyr::bind_rows(yhsl) %>%
@@ -44,6 +53,7 @@ message('  checking for full table normalization...')
 
 yhs %>% dplyr::group_by(CID) %>% dplyr::summarize(N=n()) %>% dplyr::filter(N>1) -> nonunique
 if (nrow(nonunique)>0){
+	warning('Duplicates!')
 	print(as.data.frame(nonunique))
 } else {
 	message('  --> checks OK')
